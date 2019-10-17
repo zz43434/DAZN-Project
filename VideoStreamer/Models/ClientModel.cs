7c15cbb577b9b47a7a32a23780445f121d73f9fb
zaz <zahidrahman99@gmail.com>
2019-10-17 19:27:49 +0100

    Added end-points and models

---
+++ VideoStreamer/Models/ClientModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VideoStreamer.Models
+{
+    public class ClientModel
+    {
+        public Guid ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string DeviceName { get; set; }
+    }
+}
---
+++ VideoStreamer/Models/VideoModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VideoStreamer.Models
+{
+    public class VideoModel
+    {
+        public Guid VideoId { get; set; }
+        public string VideoName { get; set; }
+        public int Length { get; set; }
+    }
+}
