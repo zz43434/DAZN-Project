36ba8d4f33cdd3cd39e6d395a3c8d20679c7dae2
zahid rahman <zahidrahman99@gmail.com>
2019-10-18 07:26:18 +0100

    create unit test stubs

---
+++ VideoStreamAPI/VideoStreamTests/AuthorizationServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VideoStreamTests
+{
+    public class AuthorizationServiceTest
+    {
+        [Fact]
+        public void ReturnUserIsAuthorized_Pass() { }
+
+        [Fact]
+        public void ReturnUserIsNotAuthorized_Pass() { }
+
+        [Fact]
+        public void AuthorizesUser_Pass() { }
+
+        [Fact]
+        public void AuthorizeUser_Fail() { }
+
+        [Fact]
+        public void UserNotFoundError_Pass() { }
+    }
+}
---
+++ VideoStreamAPI/VideoStreamTests/VideoServiceTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace VideoStreamTests
+{
+    public class VideoServiceTest
+    {
+        [Fact]
+        public void ReturnAllVideos_Pass() { }
+
+        [Fact]
+        public void ReturnSingleVideo_Pass() { }
+
+        [Fact]
+        public void ReturnVideoFromGUID_Pass() { }
+
+        [Fact]
+        public void ReturnVideoNotFoundError_Pass() { }
+    }
+}
