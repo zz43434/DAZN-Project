c607b775e77ff7822596c39249bf9e95c1f70e31
zahid rahman <zahidrahman99@gmail.com>
2019-10-21 06:22:20 +0100

    unit tests

--- VideoStreamAPI/VideoStreamTests/VideoServiceTest.cs
+++ VideoStreamAPI/VideoStreamTests/VideoServiceTest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using VideoStreamAPI.Models;
 using Xunit;
 
 namespace VideoStreamTests
@@ -6,15 +9,56 @@ namespace VideoStreamTests
     public class VideoServiceTest
     {
         [Fact]
-        public void ReturnAllVideos_Pass() { }
+        public void ReturnAllVideos_Pass()
+        {
+            var result = allVideos;
+
+            Assert.Equal(result, allVideos);
+        }
 
         [Fact]
-        public void ReturnSingleVideo_Pass() { }
+        public void ReturnSingleVideo_Pass()
+        {
+            var result = allVideos.SingleOrDefault();
+
+            Assert.IsType<VideoModel>(result);
+            Assert.Single<VideoModel>(result);
+        }
 
         [Fact]
-        public void ReturnVideoFromGUID_Pass() { }
+        public void ReturnVideoFromGUID_Pass()
+        {
+
+        }
 
         [Fact]
         public void ReturnVideoNotFoundError_Pass() { }
+
+        readonly List<VideoModel> allVideos = new List<VideoModel> {
+            new VideoModel
+            {
+                VideoId = Guid.NewGuid(),
+                VideoLength = 7,
+                VideoName = "Test1"
+            },
+            new VideoModel
+            {
+                VideoId = Guid.NewGuid(),
+                VideoLength = 6,
+                VideoName = "Test2"
+            },
+            new VideoModel
+            {
+                VideoId = Guid.NewGuid(),
+                VideoLength = 4,
+                VideoName = "Test3"
+            },
+            new VideoModel
+            {
+                VideoId = Guid.NewGuid(),
+                VideoLength = 1,
+                VideoName = "Test4"
+            }
+        };
     }
 }
